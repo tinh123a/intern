9699b53d6f39f1d9be354a0773f904381cdff4c0
tranductinh2001 <0352911750a@gmail.com>
2024-06-05 10:21:49 +0700

    finđ character in array with the  match() of matchAll()

---
+++ js/stringMatchAll.js
@@ -0,0 +1,3 @@
+let texta = " tôi yêu mèo, con mèo rất là đáng yêu. con mèo rất phổ biển";
+const iteratera = texta.matchAll("c");
+document.getElementById("matchAll").innerHTML = Array.from(iteratera)
